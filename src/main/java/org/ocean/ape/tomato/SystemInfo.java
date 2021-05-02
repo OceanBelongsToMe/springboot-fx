7e3af7a35d2397d93441d6b6abd4cfebf66c78e4
ocean <oceanwangyang@163.com>
2021-05-03 02:27:13 +0800

    spring.xml

--- src/main/java/org/ocean/ape/tomato/SystemInfo.java
+++ src/main/java/org/ocean/ape/tomato/SystemInfo.java
@@ -1,16 +1,7 @@
 package org.ocean.ape.tomato;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
 public class SystemInfo {
 
-    @Bean("SystemInfoBean")
-    public SystemInfo create()
-    {
-        return new SystemInfo();
-    }
     public static String javaVersion() {
         return System.getProperty("java.version");
     }
