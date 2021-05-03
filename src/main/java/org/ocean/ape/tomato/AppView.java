132ac3b75de44d997014c40862a29a3a518cd3f7
ocean <oceanwangyang@163.com>
2021-05-03 09:18:02 +0800

    update

--- src/main/java/org/ocean/ape/tomato/AppView.java
+++ src/main/java/org/ocean/ape/tomato/AppView.java
@@ -1,9 +1,11 @@
 package org.ocean.ape.tomato;
 
 import de.felixroske.jfxsupport.AbstractFxmlView;
+import de.felixroske.jfxsupport.FXMLView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@FXMLView("app.fxml")
 public class AppView extends AbstractFxmlView
 {
     private static final Logger LOGGER = LoggerFactory.getLogger(AppView.class);
